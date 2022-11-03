373d68dd0fe4ff6116c45f32cfa17288bc98ccdb
dbadoy4874 <dbadoy4874@gmail.com>
2022-11-03 21:11:20 +0900

    discovery: fix comment

--- discovery.go
+++ discovery.go
@@ -2,10 +2,9 @@ package gogossip
 
 // OR use name 'Registration'
 type Discovery interface {
-	// The imported DHT object must have method Gossipiers.
-	// It returns all of the peer that target of gossip protocol by array of
-	// raw addresses.
-	// The raw address validate when before send gossip.
+	// The imported DHT object must have method Gossipiers implement thread
+	// safety. It returns all of the peer that target of gossip protocol by
+	// array of raw addresses. The raw address validate when before send gossip.
 	//
 	// It'll used 'SelectRandomPeers' in Gossiper.
 	Gossipiers() []string
