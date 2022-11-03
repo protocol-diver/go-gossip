bcdf397cd379fbd82c59ebaf6fc22e3978ac2f0a
dbadoy4874 <dbadoy4874@gmail.com>
2022-11-03 21:21:20 +0900

    gossip: handle newly messages

--- gossip.go
+++ gossip.go
@@ -1,22 +1,38 @@
 package gogossip
 
 import (
-	"encoding/json"
 	"fmt"
-	"time"
 
 	lru "github.com/hashicorp/golang-lru"
 )
 
+const (
+	// cfg
+	GossipNumber = 2
+
+	// Set to twice the predicted value of messages to occur per second
+	MessageCacheSize = 0
+)
+
 type Gossiper struct {
-	latest map[string]time.Time
+	discovery Discovery
 
 	messagePipe  chan []byte
 	messageCache lru.Cache
+
+	cfg *Config
+}
+
+func (g *Gossiper) Push(data []byte) error {
+	return nil
 }
 
 func (g *Gossiper) Messages() chan []byte {
 	return g.messagePipe
+}
+
+func (g *Gossiper) SelectRandomPeers() []string {
+	return nil
 }
 
 func (g *Gossiper) handler(buf []byte) error {
@@ -24,52 +40,29 @@ func (g *Gossiper) handler(buf []byte) error {
 	if err != nil {
 		return err
 	}
-	_ = encType
+	_, _ = payload, encType
 
 	switch packetType {
-	case PullReqestType:
-		// 1. Payload decryption
-		//
-		// 2. Unmarshal
-		var request PullRequest
-		if err := json.Unmarshal(payload, &request); err != nil {
-			return err
-		}
-		// 3. Checks requested data
-		data := g.get(request.Target)
-		if data == nil {
-			_ = "skip"
-		}
-
-		// 4. Build the message
-		msg := new(PullResponse)
-		msg.SetID(idGenerator())
-		msg.Data = data
-		// 5. Send to requestor
+	case PushMessageType:
+		// 1. Store / Ignore if already exist
+		// 2. Store? -> Send PushAckType to sender
+	case PushAckType:
+		// 1. Counts number of ACK
+		// 2. Done or multicast PullSyncType
+	case PullSyncType:
+		// 1. Send PullRequest to sender
+	case PullRequestType:
+		// 1. Find requested data
+		// 2. Marshal and encryption
+		// 3. Make PullResponse
+		// 4. Send to requestor
 	case PullResponseType:
-		msg := new(PullRequest)
-		if err := json.Unmarshal(payload, &msg); err != nil {
-			return err
-		}
-		value := g.get(msg.ID())
-		if value != nil {
-			_ = "skip"
-		}
-		// temp
-		if true {
-			g.messageCache.Add(msg.ID(), []byte{})
-			g.messagePipe <- []byte{}
-		}
-		_ = value
-		// 1. Select random peers
-		// 2. Make PullRequest
-		// 3. Encryption
-		// 4. Send
+		// 1. Store / Ignore if already exist
 	}
 	return fmt.Errorf("invalid packet type %d", packetType)
 }
 
-func (g *Gossiper) get(id [8]byte) []byte {
+func (g *Gossiper) get(id uint) []byte {
 	v, ok := g.messageCache.Get(id)
 	if !ok {
 		return nil
