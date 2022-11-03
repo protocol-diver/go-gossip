a934403e3176a08c893f5a1b85d9f8f2f5e02ac9
dbadoy4874 <dbadoy4874@gmail.com>
2022-11-04 00:50:27 +0900

    transport: mock

--- transport.go
+++ transport.go
@@ -1,1 +1,13 @@
 package gogossip
+
+import (
+	"net"
+	"net/netip"
+)
+
+type Transport interface {
+	ReadFromUDP(b []byte) (n int, addr *net.UDPAddr, err error)
+
+	WriteToUDP(b []byte, addr *net.UDPAddr) (int, error)
+	WriteMsgUDPAddrPort(b []byte, oob []byte, addr netip.AddrPort) (n int, oobn int, err error)
+}
