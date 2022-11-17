e93c895fd409434c926149ec3c2156fe2d509d82
dbadoy4874 <dbadoy4874@gmail.com>
2022-11-17 20:02:36 +0900

    message: add field `to`, method `marshalPacketWithEncryption`

--- message.go
+++ message.go
@@ -1,8 +1,19 @@
 package gogossip
 
+import (
+	"net"
+)
+
 type Packet interface {
-	ID() uint32
 	Kind() byte
+
+	// Packet handler returns packet list if need respond. and basically
+	// it is used to divide and transmit a large response. Add 'to' to
+	// the packet itself, as there may be times when need to send a
+	// response to multiple peers later.
+	//
+	// It is removed in marshalling and cannot be used on the receiver side.
+	To() *net.UDPAddr
 }
 
 const (
@@ -12,17 +23,29 @@ const (
 
 type (
 	PullRequest struct {
-		id uint32
 	}
 	PullResponse struct {
-		id     uint32
+		to     *net.UDPAddr
 		Keys   [][8]byte
 		Values [][]byte
 	}
 )
 
-func (req *PullRequest) ID() uint32 { return req.id }
-func (req *PullRequest) Kind() byte { return PullRequestType }
+func marshalPacketWithEncryption(packet Packet, encType EncryptType, passphrase string) ([]byte, error) {
+	cipher, err := EncryptPacket(encType, passphrase, packet)
+	if err != nil {
+		return nil, err
+	}
+	return bytesToLabel([]byte{packet.Kind(), byte(encType)}).combine(cipher)
+}
 
-func (res *PullResponse) ID() uint32 { return res.id }
+func (req *PullRequest) Kind() byte       { return PullRequestType }
+func (req *PullRequest) To() *net.UDPAddr { panic("not supported") }
+
 func (res *PullResponse) Kind() byte { return PullResponseType }
+func (res *PullResponse) To() *net.UDPAddr {
+	if res.to == nil {
+		panic("'to' is empty (hint: maybe you are the recipient)")
+	}
+	return res.to
+}
