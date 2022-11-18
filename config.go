1b976a3020f8017e5eb5f7748dd3574c7e361a93
dbadoy4874 <dbadoy4874@gmail.com>
2022-11-18 19:15:53 +0900

    config: set default configs, add validate method

--- config.go
+++ config.go
@@ -1,8 +1,29 @@
 package gogossip
 
+import (
+	"fmt"
+)
+
 type Config struct {
-	encryptType EncryptType
-	passphrase  string
+	GossipNumber int
+	EncType      EncryptType
+	Passphrase   string
 }
 
-// gossipNumber int
+func DefaultConfig() *Config {
+	return &Config{
+		GossipNumber: 2,
+		EncType:      NON_SECURE_TYPE,
+		Passphrase:   "",
+	}
+}
+
+func (c *Config) validate() error {
+	if c.EncType.String() == "" {
+		return fmt.Errorf("invalid EncryptType %d", c.EncType)
+	}
+	if c.EncType != NON_SECURE_TYPE && c.Passphrase == "" {
+		return fmt.Errorf("Passphrase required")
+	}
+	return nil
+}
