2a55559b6e36d2ad0238df2af4787b4c18caed41
dbadoy4874 <dbadoy4874@gmail.com>
2022-11-30 16:32:38 +0900

    security: add test

--- security_test.go
+++ security_test.go
@@ -1,9 +1,44 @@
 package gogossip
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 )
+
+func TestNewCipher(t *testing.T) {
+	tds := []struct {
+		eType EncryptType
+		err   error
+	}{
+		{
+			eType: NON_SECURE_TYPE,
+			err:   nil,
+		},
+		{
+			eType: AES256_CBC_TYPE,
+			err:   nil,
+		},
+		{
+			eType: 0x03,
+			err:   errors.New("not supported encryption type"),
+		},
+	}
+
+	for _, td := range tds {
+		defer func() {
+			if err := recover(); err != nil {
+				if td.err == nil {
+					t.Fatalf("TestNewCipher failure, want: %v, got: %v", td.err, err)
+				} else {
+					if err != td.err.Error() {
+						t.Fatalf("TestNewCipher failure, want: %v, got: %v", td.err, err)
+					}
+				}
+			}
+		}()
+		newCipher(EncryptType(td.eType))
+	}
+}
 
 func TestEncrypt_AES256_CBC(t *testing.T) {
 	tds := []struct {
@@ -19,23 +54,21 @@ func TestEncrypt_AES256_CBC(t *testing.T) {
 	cipher := newCipher(AES256_CBC_TYPE)
 
 	for _, td := range tds {
-		defer func() {
-			err := recover()
-			if !td.err && err != nil {
-				fmt.Println(td.err, err)
-				t.Fatal("fil")
-			}
-		}()
 		enc, err := cipher.Encrypt(td.ekey, []byte(td.plain))
 		if err != nil {
 			t.Fatal(err)
 		}
 		dec, err := cipher.Decrypt(td.dkey, enc)
 		if err != nil {
-			t.Fatal(err)
+			if !td.err {
+				t.Fatal(err)
+			}
 		}
-		if string(dec) != td.plain {
-			panic("fil")
+
+		if !td.err {
+			if string(dec) != td.plain {
+				t.Fatalf("TestEncrypt_AES256_CBC failure, want: %s, got: %s", string(td.plain), string(dec))
+			}
 		}
 	}
 }
